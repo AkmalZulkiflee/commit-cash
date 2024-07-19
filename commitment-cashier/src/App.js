f6ccd8cd131a1a771e01eba30e1de67d8c10c466
Akmal <akmal@rstn2u.com>
2024-07-19 11:13:56 +0800

    First commit into github repo.

--- commitment-cashier/src/App.js
+++ commitment-cashier/src/App.js
@@ -10,7 +10,7 @@ function App() {
           CommitCash
         </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload...
         </p>
         <a
           className="App-link"
